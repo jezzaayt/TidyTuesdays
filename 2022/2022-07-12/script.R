d8b596c06fb64bf546cd8ce442def1d384802e1f
jezzaayt <gjaytoun@gmail.com>
2022-07-15 20:12:47 +0100

    resave

--- 2022/2022-07-12/script.R
+++ 2022/2022-07-12/script.R
@@ -12,6 +12,7 @@ names(df)[2] <- "Airports"
 
 df_graph <- df |> group_by(df$Airports) |> filter(n()>5)
 
-ggplot(df_graph, aes(x = YEAR,y = FLT_TOT_IFR_2, fill= FLT_TOT_IFR_2)) + geom_col() +facet_wrap(~Airports, nrow = 5) + theme_fivethirtyeight() + theme(legend.position = "top", legend.key.size = unit(2,"cm")) +
-     labs(title ="Flight activity between 2016-2022", fill =  "Amount of Flights", x = "Years", y = "Number of Flights", subtitle = "TidyTuesday 2022-07-12", caption = "By Jeremy A") 
+ggplot(df_graph, aes(x = YEAR,y = FLT_TOT_IFR_2, fill= FLT_TOT_IFR_2)) + geom_col() +facet_wrap(~Airports, nrow = 5) + theme_fivethirtyeight() + theme(legend.position = "top", legend.key.size = unit(2.5,"cm")) +
+     labs(title ="Flight activity between 2016-2022", fill =  "Amount of Flights", x = "Years", y = "Number of Flights", subtitle = "TidyTuesday 2022-07-12\nAll Airports that had flights between 2016-2022", 
+          caption = "By Jeremy A\nhttps://github.com/jezzaayt") 
 
